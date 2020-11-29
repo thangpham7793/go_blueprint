321d54aa9d4247dc0f6e221ddb781f06557d14a8
Thang <thangnus@gmail.com>
2020-11-29 18:38:36 +1300

    made port as command line flag

--- chat_app/main.go
+++ chat_app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -21,11 +22,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
 	//write to response
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
-
+	var addr = flag.String("addr", ":8080", "The addr of the app")
+	flag.Parse()
 	r := newRoom()
 	//pass in a pointer since the pointer implements the Handler interface, which has only the ServeHTTP method! http.HandleFunc doesn't work because it requires a function with the same signature, whicle the method pointer has 3 args (the type itself)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -34,7 +36,8 @@ func main() {
 	//starts the room in a separate thread, otherwise either the room or the server will block!
 	go r.run()
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
